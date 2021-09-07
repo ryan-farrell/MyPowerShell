4ba12b64618bb2c5abced08526040707c9615624
Ryan <ryan.farrell44@gmail.com>
2021-09-07 21:18:08 +0100

    New theme and settings updated

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -5,13 +5,13 @@
 # ****************** #
 # * Import Modules * #
 # ****************** #
-# Import-Module posh-git
+Import-Module posh-git
 Import-Module oh-my-posh
 
 # Load my own custom functions at startup
 $OwnFunctionsDir = "$env:USERPROFILE\Documents\MyPowerShell\functions"
 
-Write-Host " Loading my own PowerShell functions from: $OwnFunctionsDir " -ForegroundColor Black -BackgroundColor Cyan
+Write-Host " Loading my own PowerShell functions from: $OwnFunctionsDir " -ForegroundColor White -BackgroundColor DarkGray
 Get-ChildItem "$OwnFunctionsDir\*.ps1" | ForEach-Object{.$_}
 Write-Host '_-_-_-_-_-_--_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_'
 Write-Host '' # Empty line
@@ -21,7 +21,7 @@ Write-Host '' # Empty line
 # ****************************************************** #
 # Update this next line with the theme name to load i.e. Set-PoshPrompt -Theme [theme-name]
 # Check out the readme in C:\Users\ryanf\Documents\MyPowerShell\README.md for more details
-Set-PoshPrompt -Theme slim
+Set-PoshPrompt -Theme neon_burn_rf44
 
 # *************************************************************************************************************************************** #
 # DON'T AMEND THIS FILE. THE FILE TO AMEND IS IN $PROFILE  =  C:\Users\<username>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
